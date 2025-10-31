ea3c7f6fe2f14f443a3b6f1b85187cbaee0e55b3
Pravin A <pravindev666@gmail.com>
2025-11-01 01:13:31 +0530

    Update iam.tf

--- multi-region-dr-system/ infrastructure/terraform/iam.tf
+++ multi-region-dr-system/ infrastructure/terraform/iam.tf
@@ -1,1 +1,190 @@
+# IAM configurations are included in lambda-functions.tf
+# This file contains additional IAM roles and policies
 
+# IAM Role for Failover Orchestrator
+resource "aws_iam_role" "failover_orchestrator_role" {
+  name = "dr-failover-orchestrator-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for Failover Orchestrator
+resource "aws_iam_role_policy" "failover_orchestrator_policy" {
+  name = "dr-failover-orchestrator-policy"
+  role = aws_iam_role.failover_orchestrator_role.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:GetHealthCheck",
+          "route53:GetHealthCheckStatus",
+          "route53:ListHealthChecks",
+          "route53:UpdateHealthCheck",
+          "route53:ChangeResourceRecordSets",
+          "route53:GetChange"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ]
+        Resource = aws_dynamodb_table.mumbai.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData",
+          "cloudwatch:GetMetricStatistics"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticloadbalancing:DescribeLoadBalancers",
+          "elasticloadbalancing:DescribeTargetHealth"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# IAM Role for Data Consistency Validator
+resource "aws_iam_role" "data_validator_role" {
+  name = "dr-data-validator-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for Data Consistency Validator
+resource "aws_iam_role_policy" "data_validator_policy" {
+  name = "dr-data-validator-policy"
+  role = aws_iam_role.data_validator_role.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:DescribeTable",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = [
+          aws_dynamodb_table.mumbai.arn,
+          "${aws_dynamodb_table.mumbai.arn}/index/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# IAM Role for CloudWatch Events
+resource "aws_iam_role" "cloudwatch_events_role" {
+  name = "dr-cloudwatch-events-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Policy for CloudWatch Events
+resource "aws_iam_role_policy" "cloudwatch_events_policy" {
+  name = "dr-cloudwatch-events-policy"
+  role = aws_iam_role.cloudwatch_events_role.id
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
