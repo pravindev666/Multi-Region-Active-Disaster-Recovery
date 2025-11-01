b6289031483090a161b6a54ee746081e83797551
Pravin A <pravindev666@gmail.com>
2025-11-01 08:29:33 +0530

    Update variables.tf

--- multi-region-dr-system/ infrastructure/terraform/variables.tf
+++ multi-region-dr-system/ infrastructure/terraform/variables.tf
@@ -1,1 +1,51 @@
-enter variables
+variable "primary_region" {
+  description = "Primary AWS region (Mumbai)"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "secondary_region" {
+  description = "Secondary AWS region (Singapore)"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "domain_name" {
+  description = "Domain name for Route 53"
+  type        = string
+}
+
+variable "notification_email" {
+  description = "Email address for SNS notifications"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB global table"
+  type        = string
+  default     = "dr-application-data"
+}
+
+variable "lambda_runtime" {
+  description = "Lambda runtime version"
+  type        = string
+  default     = "python3.9"
+}
+
+variable "lambda_timeout" {
+  description = "Lambda function timeout in seconds"
+  type        = number
+  default     = 30
+}
+
+variable "lambda_memory_size" {
+  description = "Lambda function memory in MB"
+  type        = number
+  default     = 256
+}
